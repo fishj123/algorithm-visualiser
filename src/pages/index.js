cb2f43867ccf7397052f5a433880ad0c7ea85d8b
Fisher <jackfisher94@live.co.uk>
2019-09-17 16:34:36 +0100

    inital commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,68 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+
+  const [arr, setArr] = useState([])
+
+  const generateArray = () => {
+    const count = 100;
+    const a = []
+    for(var i=0; i < count; i++) {
+      a.push(i)
+    }
+
+    shuffleArray(a);
+
+  }
+
+  const shuffleArray = (array) => {
+    let counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+      // Pick a random index
+      let index = Math.floor(Math.random() * counter);
+
+      // Decrease counter by 1
+      counter--;
+
+      // And swap the last element with it
+      let temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+    }
+
+    setArr(array);
+  }
+
+ const doTheArrayWork = () => {
+    generateArray();
+  }
+
+  useEffect(() => {
+    doTheArrayWork()
+  }, [])
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Bubble Sort</h1>
+      <ul>
+        {arr.map(item => 
+          <li key={item} style={{width: item + "%"}} className="bubble-item" data-id={item} />
+          )}
+      </ul>
+      <button onClick={() => doTheArrayWork()}>
+        Generate New Array
+      </button>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
